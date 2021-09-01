27b4f9ba23e086c1fa84f83776f5dacb0296da02
alexandreleao <erdleosalgueiro@gmail.com>
2021-09-01 12:00:09 -0300

    intro a javascript

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('hola mundo')
+document.addEventListener("DOMContentLoaded", function(e) {
+
+console.warn('hola mundo', 'teste')
+
+const nome = prompt('Escreva seu nome')
+
+console.log(nome)
+
+
+document.getElementsByTagName('h1')
+
+const arrayTagsH1 = document.getElementsByTagName('h1')
+
+arrayTagsH1[0].innerHTML = "Mudança desde o JS"
+
+
+arrayTagsH1[0].addEventListener('click', ()=>{ alert('Adicionando um evento ao clicar')})
+
+const novoElemento = document.createElement('h2');
+
+novoElemento.innerHTML = "Alexandre"
+
+document.body.appendChild(novoElemento);
+
+})
